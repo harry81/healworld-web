31065b9b042fcbb5314c510d5548d5b869a54b1c
Hyunmin Choi <pointer81@gmail.com>
2016-11-08 23:44:47 +0900

    Addded list page module

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { HomePage } from '../home/home';
+import { ListPage } from '../list/list';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
 
@@ -13,6 +14,7 @@ export class TabsPage {
   tab1Root: any = HomePage;
   tab2Root: any = AboutPage;
   tab3Root: any = ContactPage;
+  tab4Root: any = ListPage;
 
   constructor() {
 
