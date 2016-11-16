f16a01d038a53701b593c2a22acbecfd3a0aa8fa
Hyunmin Choi <pointer81@gmail.com>
2016-11-16 17:06:41 +0900

    added form validators

--- src/pages/post/post.ts
+++ src/pages/post/post.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Validators, FormBuilder } from '@angular/forms';
 import { ItemService } from '../../providers/item-service';
 
 @Component({
@@ -9,20 +10,41 @@ import { ItemService } from '../../providers/item-service';
 })
 export class PostPage {
     public preview: any;
+    public postForm: any;
 
     constructor(public navCtrl: NavController,
-                public itemService: ItemService) {}
+                public itemService: ItemService,
+                private formBuilder: FormBuilder) {
+
+        this.postForm = this.formBuilder.group({
+            'memo': [
+                '', Validators.compose([Validators.required])
+            ],
+            'price': [
+                '3000', Validators.compose([Validators.required])
+            ],
+            'itemshot': [
+                ''
+            ]
+        });
+    }
 
     ionViewDidLoad() {
         console.log('Hello PostPage Page');
     }
 
-    onPost(input) {
+    onPostImage(input) {
         this.itemService.postImage(input.files[0])
             .then(data => {
                 console.log('response', data);
                 this.preview = data.itemshot.thumbnail__300x200;
+                this.postForm.value['itemshot'] = data.id;
+                console.log('postForm', this.postForm);
             });
     }
 
+    onSubmit() {
+        console.log('submit', this.postForm.value);
+        this.itemService.postItem(this.postForm.value);
+    }
 }
