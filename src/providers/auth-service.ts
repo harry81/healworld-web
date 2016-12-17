93edb66f547e459532be0a2254d6507acdaf4f00
Hyunmin Choi <pointer81@gmail.com>
2016-12-17 11:51:49 +0900

    added auth service

---
+++ src/providers/auth-service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import {tokenNotExpired} from 'angular2-jwt';
+import 'rxjs/add/operator/map';
+
+/*
+  Generated class for the AuthService provider.
+
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class AuthService {
+
+    constructor(public http: Http) {
+        console.log('Hello AuthService Provider');
+    }
+
+    public static authenticated() {
+        return tokenNotExpired();
+    }
+
+}
