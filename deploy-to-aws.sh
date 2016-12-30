7f334337c421d7c8953a49be13614820e4b64818
Hyunmin Choi <pointer81@gmail.com>
2016-12-30 13:45:11 +0900

    added deployment script

--- deploy-to-aws.sh
+++
@@ -1,8 +0,0 @@
-echo "Step 1 : Building code."
-npm run build --prod
-
-echo "Step 2: Putting to AWS."
-aws s3 sync  www s3://www.healworld.co.kr/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --profile pointer --region ap-northeast-2;
-
-echo "Step 3: Invalidating cloudfront"
-aws cloudfront create-invalidation --invalidation-batch file://invbatch.json --distribution-id=E1WSE4OG309BMK
---
+++ fabfile.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+import re
+import json
+from fabric.api import local
+
+def deploy_to_aws():
+    print 'Step 1 : version up'
+    version_up()
+
+    print 'Step 2 : Building code.'
+    build()
+
+    print 'Step 3 : Putting to AWS.'
+    copy_to_aws()
+
+    print 'Step 4 : Invalidating cloudfront.'
+    invalidate_cloudfront()
+
+def build():
+    local("npm run build --prod")
+
+def copy_to_aws():
+    local("aws s3 sync  www s3://www.healworld.co.kr/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --profile pointer --region ap-northeast-2;")
+
+def invalidate_cloudfront():
+    local("aws cloudfront create-invalidation --invalidation-batch file://invbatch.json --distribution-id=E1WSE4OG309BMK")
+
+def version_up():
+    version_up_invbatch()
+    version_up_service_worker()
+
+def version_up_invbatch():
+    with open('invbatch.json', 'rt') as fp:
+        content= fp.read()
+
+    content_json = json.loads(content)
+    reference = content_json['CallerReference']
+    reference = int(reference) + 1
+    content_json['CallerReference'] = str(reference)
+
+    with open('invbatch.json', 'wt') as fp:
+        fp.write(json.dumps(content_json))
+
+
+def version_up_service_worker():
+    with open('src/service-worker.js', 'rt') as fp:
+        content = fp.read()
+
+    version = re.search(r'const CACHE_VERSION = (\d+)', content).group(1)
+    version = int(version) + 1
+    print "CACHE_VERSION = %d" % version
+    content = re.sub(r'const CACHE_VERSION = (\d+)', r'const CACHE_VERSION = %d' % version, content)
+
+    with open('src/service-worker.js', 'wt') as fp:
+        fp.write(content)
