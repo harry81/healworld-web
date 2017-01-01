85d021c5b1ed555096c104248e31559add7928f4
Hyunmin Choi <pointer81@gmail.com>
2017-01-01 14:35:31 +0900

    use npm run for building

--- fabfile.py
+++ fabfile.py
@@ -17,7 +17,7 @@ def deploy_to_aws():
     invalidate_cloudfront()
 
 def build():
-    local("ionic build")
+    local("npm run build --prod --aot")
 
 def copy_to_aws():
     local("aws s3 sync  www s3://www.healworld.co.kr/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --profile pointer --region ap-northeast-2;")
