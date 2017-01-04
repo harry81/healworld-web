92a9915fe08c911f9824821c4f0ac1a9a91fbd16
Hyunmin Choi <pointer81@gmail.com>
2017-01-04 22:46:43 +0900

    align comment

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -102,7 +102,6 @@ export class DetailPage {
     }
 
     postComment() {
-        console.log('in postcomment');
         if (!this.authService.isAuthorized()) {
             alert("[정보] 로그인후 댓글을 달 수 있습니다.");
             return;
