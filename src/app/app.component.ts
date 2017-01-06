3dd108ba0f5194f7d46ade3c54a67770369d6ba1
Hyunmin Choi <pointer81@gmail.com>
2017-01-06 16:44:30 +0900

    remove packages from ionic-native

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
-import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { ListPage } from '../pages/list/list';
 import { ProfilePage } from '../pages/profile/profile';
@@ -60,8 +59,8 @@ export class MyApp {
         this.platform.ready().then(() => {
             // Okay, so the platform is ready and our plugins are available.
             // Here you can do any higher level native things you might need.
-            StatusBar.styleDefault();
-            Splashscreen.hide();
+            // StatusBar.styleDefault();
+            // Splashscreen.hide();
             this.registerBackButtonListener();
         });
     }
