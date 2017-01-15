451f2668b2fe95f87f90ba2ec0dff2b1ea5b4782
Hyunmin Choi <pointer81@gmail.com>
2017-01-15 18:50:48 +0900

    center logout button

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,5 +1,5 @@
 // tick this to make the cache invalidate and update
-const CACHE_VERSION = 160;
+const CACHE_VERSION = 161;
 const CURRENT_CACHES = {
   'read-through': 'read-through-cache-v' + CACHE_VERSION
 };
