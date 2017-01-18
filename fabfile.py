0e9fa22adb1ef0d14b115a430922d06c56ad2287
Hyunmin Choi <pointer81@gmail.com>
2017-01-18 12:44:58 +0900

    run sw-precache during building process
    to support offline first

--- fabfile.py
+++ fabfile.py
@@ -3,12 +3,16 @@ import re
 import json
 from fabric.api import local
 
+
 def deploy_to_aws():
     print 'Step 1 : version up'
     version = version_up()
 
     print '\nStep 2 : Building code.'
     build()
+
+    print '\nStep 3-1 : extra for a while.'
+    local("sw-precache --root=www --config=src/sw-precache-config.json --verbose")
 
     print '\nStep 3 : Putting to AWS.'
     copy_to_aws()
@@ -18,23 +22,28 @@ def deploy_to_aws():
 
     print 'version %s' % version
 
+
 def build():
     local("npm run build --prod --aot")
+
 
 def copy_to_aws():
     local("aws s3 sync  www s3://www.healworld.co.kr/ --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --profile pointer --region ap-northeast-2;")
 
+
 def invalidate_cloudfront():
     local("aws cloudfront create-invalidation --invalidation-batch file://invbatch.json --distribution-id=E1WSE4OG309BMK")
 
+
 def version_up():
     version_up_invbatch()
-    version = version_up_service_worker()
-    return version
+    # version = version_up_service_worker()
+    # return version
+
 
 def version_up_invbatch():
     with open('invbatch.json', 'rt') as fp:
-        content= fp.read()
+        content = fp.read()
 
     content_json = json.loads(content)
     reference = content_json['CallerReference']
