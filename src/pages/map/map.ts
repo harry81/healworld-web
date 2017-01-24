735d873b4aca3cacc56f7dc7064847465873fc09
Hyunmin Choi <pointer81@gmail.com>
2017-01-24 20:18:22 +0900

    show current location on map

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -10,12 +10,18 @@ export class MapPage {
     public lng: number;
     public zoom: number;
 
+    public coord: {lat:string, lan:string};
+
     constructor(public navCtrl: NavController,
                 public params:NavParams) {
 
         this.lat = params.get('lat');
         this.lng = params.get('lng');
         this.zoom = params.get('zoom');
+
+        let position = sessionStorage.getItem('position');
+        let parse_position = JSON.parse(position);
+        this.coord = parse_position;
     }
 
     ionViewDidLoad() {
